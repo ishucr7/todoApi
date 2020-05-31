04509b1fc3171fe2e575518c9b5ee6182b8d2a05
Mohd Ali Rizwi <mohdalirizwi01@gmail.com>
2020-06-01 03:22:09 +0530

    User jwt code added

---
+++ app/models/user.model.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, Sequelize, Role) => {
+    const User = sequelize.define("users", {
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        // allowNull: false,
+        references: {
+            model: Role,
+            key: 'id',
+        }
+      }
+    },
+    {
+        timestamps: true
+    });
+  
+    return User;
+  };
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var corsOptions = {
 };
 
 const db = require("./app/models/index.js");
+const Role = db.role;
 // db.sequelize.sync();
 
 app.use(cors(corsOptions));
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Drop the tables and resync
 db.sequelize.sync({ force: true }).then(() => {
     console.log("Drop and re-sync db.");
+    initial();
 });
 
 // simple route
@@ -29,9 +31,23 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "admin"
+  });
+}
