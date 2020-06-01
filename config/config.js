27127640ebf0faf2311dba000834c85753481027
ishucr7 <aashish.gaba097@gmail.com>
2020-06-01 13:39:01 +0530

    Add environment configuration

--- config/config.js
+++ config/config.js
@@ -1,16 +1,13 @@
-{
+require('dotenv').config();
+module.exports = {
     "development": {
-        "username": "root",
-        "password": "Radhaswami1",
-        "database": "todoApi",
-        "host": "127.0.0.1",
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
         "dialect": "mysql"
     },
     "test": {
-        "username": "root",
-        "password": "Radhaswami1",
-        "database": "todoApi",
-        "host": "127.0.0.1",
         "dialect": "mysql"
     },
     "production": {
