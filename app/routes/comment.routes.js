f63adb5050d61e46ecde2582125e9eb94803fa51
Mohd Ali Rizwi <mohdalirizwi01@gmail.com>
2020-06-07 17:53:55 +0530

    Updated routes

--- app/routes/comment.routes.js
+++ app/routes/comment.routes.js
@@ -11,15 +11,12 @@ module.exports = app => {
     router.put("/:id", comments.update);    
     
     // Get all comments for a task
-    router.get("/:id", comments.getAll);    
-    // CHANGE TO api/task/:id/comments 
+    app.get("api/task/:id/comments", comments.getAll);    
 
     // Delete a comment with id
     router.delete("/:id", comments.destroy);    
 
-    app.use('/api/user/comments', [authJwt.verifyToken]);
-    app.use('/api/user/comments', router);
-
-    //  Remove users from all routes api/comments/
+    app.use('/api/comments', [authJwt.verifyToken]);
+    app.use('/api/comments', router);
 
   };
