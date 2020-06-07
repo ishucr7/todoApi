94f143481a0604844acb270965b2f848519a6422
Mohd Ali Rizwi <mohdalirizwi01@gmail.com>
2020-06-07 20:51:22 +0530

    Updated email config

--- app/config/email.config.js
+++ app/config/email.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "e74949e04501a1",
-      pass: "6237fb096bb6ad"
-    }
+  service: "gmail",
+  host: "smtp.google.com",
+  secure: false,
+  port: 587,
+  auth: {
+      user: "ali.emailservice8@gmail.com",
+      pass: "noone@123"
+  }
 };
+
+// host: "smtp.mailtrap.io",
+// port: 2525,
+// auth: {
+//   user: "e74949e04501a1",
+//   pass: "6237fb096bb6ad"
+// }
